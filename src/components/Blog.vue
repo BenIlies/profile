d11d8250ccdb731352e9c42cf11c16056e066607
benilies <iliesbenh@gmail.com>
2025-02-07 16:07:43 +0300

    add notion blockpage

--- src/components/Blog.vue
+++ src/components/Blog.vue
@@ -1,3 +1,21 @@
-<template></template>
+<template>
+  <NotionRenderer :blockMap="blockMap" fullPage />
+</template>
 
-<style scoped></style>
+<script lang="ts">
+import { NotionRenderer, getPageBlocks } from "vue-notion";
+
+export default {
+  components: { NotionRenderer },
+  data: () => ({ blockMap: null }),
+  async created() {
+    // get Notion blocks from the API via a Notion pageId
+    this.blockMap = await getPageBlocks("19375c89bc778031b015dc2ef9d2aa40");
+  },
+};
+</script>
+
+<style>
+/* optional Notion-like styles */
+@import "vue-notion/src/styles.css";
+</style>
